5358027bbfaf955c70694b9e30902696435824f9
Denise Ugalde <vanesa_7_denise@hotmail.com>
2026-09-02 09:48:46 -0600

    Navbar, home y pagina de inicio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_01_164638) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_212744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,9 +46,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_01_164638) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "city"
+    t.string "country"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name"
+    t.string "name"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
