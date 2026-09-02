f3865ea22182b270fa858a862b2bc6e1de01ead3
Mitzi Yael Santos Juárez <yaelsantos@outlook.com>
2026-09-02 15:16:28 -0600

    schedules

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,38 @@
+class SchedulesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_schedule, only: [:edit, :update, :destroy]
+
+  def edit
+  end
+
+  def update
+    if @schedule.update(schedule_params)
+      redirect_to dashboard_path, notice: "¡Ajuste de manecillas! Tu evento ha cambiado de rumbo."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @schedule.destroy
+    redirect_to dashboard_path, alert: "Despejado por aquí. ¡Adiós a este compromiso!"
+  end
+
+  private
+
+  def set_schedule
+    @schedule = current_user.schedules.find(params[:id])
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(
+      :title,
+      :description,
+      :category,
+      :starting_date,
+      :starting_hour,
+      :ending_date,
+      :ending_hour
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :schedules, only: [:edit, :update, :destroy]
+  get 'dashboard', to: 'pages#dashboard'
 
 end
