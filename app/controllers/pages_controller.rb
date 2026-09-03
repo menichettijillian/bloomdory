88306fa582e1d71fb7da88bb67a1cb4a73b29618
Mitzi Yael Santos Juárez <yaelsantos@outlook.com>
2026-09-03 15:37:42 -0600

    Schedules-view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,28 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
   end
 
   def dashboard
-    # Carga los chats del usuario actual (o un array vacío si aún no tienes el modelo Chat)
     @chats = current_user.chats rescue []
   end
 
-  def myweek
+  def schedules
+    @schedules = current_user.schedules
+
+    base_date = Date.new(2025, 5, 12)
+    start_of_week = base_date.beginning_of_week(:monday)
+
+    @week_days = (0..6).map do |i|
+      day = start_of_week + i.days
+      {
+        name: day.strftime("%a").upcase,
+        num: day.strftime("%d"),
+        date: day
+      }
+    end
+
+    @week_range_text = "#{start_of_week.strftime("%d")} - #{(start_of_week + 6.days).strftime("%d %B")}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
+
   resources :schedules, only: [:edit, :update, :destroy]
-  get 'dashboard', to: 'pages#dashboard'
 
   resources :chats, only: [:show, :create] do
     resources :messages, only: [:create]
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
 
-  get "myweek", to: "pages#myweek", as: :my_week
-
+  get "schedules", to: "pages#schedules", as: :schedules
 end
